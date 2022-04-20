4bc16ed81e4a6cf56c523ba68fde1293f462beba
Dimon <dmezey@mail.ua>
2022-04-20 18:17:33 +0300

    mergre

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,8 +1,8 @@
-import { CorrectNews } from './correctNews/index';
-export {Main} from './main'
-export {TabNews} from './tab_news'
-export {Metainfo} from './metainfo'
-export {CorrectNews} from './correctNews'
-export {Admin} from './admin'
-export {CreatePage} from './create'
+import { CorrectNews } from "./correctNews/index";
+export { Main } from "./main";
+export { TabNews } from "./tab_news";
+export { Metainfo } from "./metainfo";
+export { CorrectNews } from "./correctNews";
+export { Admin } from "./admin";
+export { CreatePage } from "./create";
 export { Login } from "./login";
