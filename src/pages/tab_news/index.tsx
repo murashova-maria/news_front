ee32478168d20405f3665f35c5b17373fdd00c3a
OnePeace30 <super@web-developer.space>
2022-04-20 23:41:16 +0200

    only tab news on tab

--- src/pages/tab_news/index.tsx
+++ src/pages/tab_news/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { TopCards } from '../../components/Cards/TopCards'
 import { InfoBlock } from '../../components/InfoBlock/InfoBlock'
 import { useHttp } from "../../hooks/useHttp";
@@ -14,24 +14,11 @@ export const TabNews: React.FC = () => {
     const [tabs, setTabs] = useState<TabType[]>([]);
     const location = useLocation();
     const { request } = useHttp();
+    const history = useNavigate()
     useEffect(() => {
         const query = new URLSearchParams(location.search);
-        const tab = query.get('tab');
-        if (tab) setTab(Number(tab));
-        (async function () {
-            const resp: MainType | null = await request({
-              path: "/main/",
-              method: "GET",
-            });
-            if (resp) setMain(resp);
-          })();
-        (async function () {
-            const resp: MainType[] | null = await request({
-              path: "/secondary_main/",
-              method: "GET",
-            });
-            if (resp) setSecondaryMain(resp);
-          })();
+        const tab_id = query.get('tab');
+        if (tab_id) setTab(Number(tab_id));
         (async function () {
             const resp: TabType[] | null = await request({
               path: "/tabs/",
@@ -40,6 +27,25 @@ export const TabNews: React.FC = () => {
             if (resp) setTabs(resp);
           })();
     }, [location]);
+
+    useEffect(() => {
+        if (tab){
+            (async function () {
+                const resp: MainType | null = await request({
+                path: "/main/?tab=" + tab,
+                method: "GET",
+                });
+                if (resp) setMain(resp);
+            })();
+            (async function () {
+                const resp: MainType[] | null = await request({
+                path: "/secondary_main/?tab=" + tab,
+                method: "GET",
+                });
+                if (resp) setSecondaryMain(resp);
+            })();
+        }
+    }, [tab])
 
     useEffect(() => {
         (async function () {
@@ -56,7 +62,7 @@ export const TabNews: React.FC = () => {
         <>
             <div className='main__section0'>
                 <div className="main__section0_left">
-                    <div className={`bigCard ${main?.tab.replace('&', '')}`}>
+                    <div className={`bigCard ${main?.tab.replace('&', '')}`} onClick={() => history({pathname:`/news?newsid=${main?.id}`})}>
                         <div className="bigCard__img">
                             <div className={`tag ${main?.tab.replace('&', '')}`}>{main?.tab}</div>
                             <img src={main?.media_link} alt="newsItems" />
