f09e70c4143cfb17f842b99d18913e26e80c90fd
Dimon <dmezey@mail.ua>
2022-04-25 14:56:57 +0300

    fix: links and checked

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -6,26 +6,38 @@ import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { useEffect } from "react";
 
 interface IDropdown {
+  value?: string;
   label: string;
   options: { value: string; name: string }[];
   handleChange: (value: string, name: string) => void;
 }
 
 export const Dropdown: React.FC<IDropdown> = ({
+  value,
   label,
   options,
   handleChange,
 }) => {
-  const [selected, setSelected] = React.useState({ value: "", name: "" });
+  const [selected, setSelected] = React.useState({
+    value: "",
+    name: "",
+  });
 
   const handleChg = (event: SelectChangeEvent) => {
     setSelected({ value: event.target.value, name: event.target.name });
   };
 
   useEffect(() => {
+    const select = options.find((item) => item.value === value);
+    if (select) setSelected({ value: select.value, name: select.name });
+  }, [value]);
+
+  useEffect(() => {
     const select = options.find((item) => item.value === selected.value);
-    if (select) handleChange(select.value, select.name);
-  }, [selected]);
+    if (select) {
+      handleChange(select.value, select.name);
+    }
+  }, [selected, value]);
 
   return (
     <FormControl sx={{ m: 1, minWidth: 200 }} size="small">
