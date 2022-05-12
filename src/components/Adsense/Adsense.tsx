0519f1e4f40788842e574862f67c3806c4e4c933
npo <npo@exante.eu>
2022-05-12 12:22:47 +0500

    NEWS-8 adsense for google

--- src/components/Adsense/Adsense.tsx
+++ src/components/Adsense/Adsense.tsx
@@ -16,20 +16,20 @@ export const Adsense: FC<Props> = ({slot, isHorisontal = false}) => {
         }, 2000)
     }, []);
 
+    const stylesHorisontal= {display: "inline-block", width: '1000px', height: '350px'}
+    const stylesVertical = {display: "inline-block", width: '360px', height: '1200px'}
 
     return <div style={{width: "100%", height: "100%"}}>
         <script async
                                src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${clientId}`}
                                crossOrigin="anonymous"/>
         <ins className="adsbygoogle"
-             style={{display: "block", height: "100%", width: "100%"}}
-             data-ad-format="autorelaxed"
+             style={isHorisontal ? stylesHorisontal : stylesVertical}
              data-ad-client={`ca-pub-${clientId}`}
              data-ad-slot={slot}
-             data-matched-content-rows-num={isHorisontal ? "1, 4" : "8, 8"}
-             data-matched-content-columns-num={isHorisontal ? "8, 1" : "1, 1"}
-             data-matched-content-ui-type="image_sidebyside, image_card_sidebyside"
+             // data-matched-content-rows-num={isHorisontal ? "1, 4" : "8, 8"}
+             // data-matched-content-columns-num={isHorisontal ? "8, 1" : "1, 1"}
+             // data-matched-content-ui-type="image_sidebyside, image_card_sidebyside"
         />
-
     </div>
 }
