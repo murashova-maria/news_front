7e285a709c3986bb13489e90ef763e9414bf375e
Eddy <edkosyan2000@gmail.com>
2022-07-06 12:09:37 +0400

    Add users perms configuration

--- src/components/BlockAdminCard/BlockAdminCard.tsx
+++ src/components/BlockAdminCard/BlockAdminCard.tsx
@@ -1,14 +1,53 @@
-import React, {useState} from "react";
+import React, { useMemo, useState } from "react";
 import { AdminCard } from "./AdminCard/AdminCard";
-import { Status } from "../../types";
-import InfiniteScroll from 'react-infinite-scroll-component';
+import { PermissionType, Status } from "../../types";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+///////////////////////////
+
+
+
+
+// Hardcored permissions for users
+const permissionsList: { [key: string]: PermissionType[] } = {
+  "a001@fastmail.nl": [
+    "decline",
+    "edit",
+    "publish",
+    "delete",
+    "restore",
+    "unpublish",
+  ],
+  "a002@fastmail.nl": [
+    "decline",
+    "edit",
+    "publish",
+    "delete",
+    "restore",
+    "unpublish",
+  ],
+  "editor1@fastmail.nl": ["decline", "edit", "publish", "delete", "restore"],
+  "editor2@fastmail.nl": ["decline", "edit", "publish", "delete", "restore"],
+  "editor3@fastmail.nl": ["decline", "edit", "publish", "delete", "restore"],
+  "viewer1@fastmail.nl": [],
+  "viewer2@fastmail.nl": [],
+};
+
+const checkPermissions = (email: string | null): PermissionType[] => {
+  if (email && permissionsList[email]) {
+    return permissionsList[email];
+  }
+  return ["decline", "edit", "publish", "delete", "restore", "unpublish"];
+};
+
+///////////
 
 interface IBlockAdminCard {
   items: any;
   status: string;
   handleClick: (id: number, status: Status) => void;
-  featchItems?: (offset: number) => void
-  hasMore: boolean
+  featchItems?: (offset: number) => void;
+  hasMore: boolean;
 }
 
 export const BlockAdminCard = ({
@@ -16,27 +55,37 @@ export const BlockAdminCard = ({
   status,
   handleClick,
   featchItems,
-  hasMore
+  hasMore,
 }: IBlockAdminCard) => {
   const [offset, setOffset] = useState<number>(5);
   const next = () => {
-    if(featchItems) featchItems(offset)
-    setOffset((prev) => prev + 5)
-  }
+    if (featchItems) featchItems(offset);
+    setOffset((prev) => prev + 5);
+  };
+
+  //Permissions for buttons
+  let userInfoRaw: null | string = sessionStorage.getItem("userInfo");
+  let userEmail: null | string = null;
+  if (userInfoRaw) userEmail = JSON.parse(userInfoRaw).email;
+
+  const permissions: PermissionType[] = useMemo(() => {
+    return checkPermissions(userEmail);
+  }, [userInfoRaw]);
+
 
   return (
-    <div className="blockAdminCard" id={status} >
+    <div className="blockAdminCard" id={status}>
       <InfiniteScroll
-          height={600}
-          dataLength={items.length} //This is important field to render the next data
-          next={next}
-          hasMore={!hasMore}
-          loader={<h4>Loading...</h4>}
-          scrollableTarget={status}
+        height={600}
+        dataLength={items.length} //This is important field to render the next data
+        next={next}
+        hasMore={!hasMore}
+        loader={<h4>Loading...</h4>}
+        scrollableTarget={status}
       >
-      {items.map((el: any) => (
-        <AdminCard item={{ ...el, status }} handleClick={handleClick} />
-      ))}
+        {items.map((el: any) => (
+          <AdminCard item={{ ...el, status }} handleClick={handleClick} permissions={permissions} />
+        ))}
       </InfiniteScroll>
     </div>
   );
