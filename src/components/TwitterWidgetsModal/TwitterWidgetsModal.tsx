22cabf60654acd91b329bf43a7320a16cb8662bf
Eddy <edkosyan2000@gmail.com>
2022-11-17 15:26:10 +0400

    redesign tweets modal in admin

--- src/components/TwitterWidgetsModal/TwitterWidgetsModal.tsx
+++ src/components/TwitterWidgetsModal/TwitterWidgetsModal.tsx
@@ -12,9 +12,9 @@ const TwitterWidgetsModal = () => {
 
     return <div className={s.Container}>
         <h2 className={s.Title}>Twitter accounts</h2>
-        <Input placeholder="First widget URL" onChange={(e: any) => setFirstLink(e.target.value)} value={firstLink} />
-        <Input placeholder="Second widget URL" onChange={(e: any) => setSecondLink(e.target.value)} value={secondLink} />
-        <Input placeholder="Third widget URL" onChange={(e: any) => setThirdLink(e.target.value)} value={thirdLink} />
+        <Input placeholder="Tweet ID" onChange={(e: any) => setFirstLink(e.target.value)} value={firstLink} />
+        <Input placeholder="Tweet ID" onChange={(e: any) => setSecondLink(e.target.value)} value={secondLink} />
+        <Input placeholder="Tweet ID" onChange={(e: any) => setThirdLink(e.target.value)} value={thirdLink} />
         <Button onClick={() => {}}>Save</Button>
     </div>
 }
