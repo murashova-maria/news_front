5fd6aeb7dc5f196b98c608585b33cebb6c38864e
Eddy <edkosyan2000@gmail.com>
2022-11-18 11:05:44 +0400

    add tweets links adding functionality in admin

--- src/components/TwitterWidgetsModal/TwitterWidgetsModal.tsx
+++ src/components/TwitterWidgetsModal/TwitterWidgetsModal.tsx
@@ -1,22 +1,76 @@
-import { useState } from 'react'
-import { Input } from '../Input/Input'
-import { Button } from '../shared/Button/Button'
-import s from './TwitterWidgetsModal.module.scss'
-
+import { useCallback, useEffect, useState } from "react";
+import { useHttp } from "../../hooks/useHttp";
+import { Input } from "../Input/Input";
+import { Button } from "../shared/Button/Button";
+import s from "./TwitterWidgetsModal.module.scss";
 
 const TwitterWidgetsModal = () => {
+  const { loading, request } = useHttp();
+  const [links, setLinks] = useState<{ [key: string]: string }>({});
 
-    const [firstLink, setFirstLink] = useState<string>('')
-    const [secondLink, setSecondLink] = useState<string>('')
-    const [thirdLink, setThirdLink] = useState<string>('')
+  const getTweetIds = useCallback(async () => {
+    try {
+      const resp = await request({
+        path: "/tw_post_links/",
+        method: "GET",
+      });
+      if (resp) {
+        const obj: any = {};
+        resp.forEach((link: any) => {
+          obj[link.id] = link.link;
+        });
+        setLinks(obj);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }, [request])
 
-    return <div className={s.Container}>
-        <h2 className={s.Title}>Twitter accounts</h2>
-        <Input placeholder="Tweet ID" onChange={(e: any) => setFirstLink(e.target.value)} value={firstLink} />
-        <Input placeholder="Tweet ID" onChange={(e: any) => setSecondLink(e.target.value)} value={secondLink} />
-        <Input placeholder="Tweet ID" onChange={(e: any) => setThirdLink(e.target.value)} value={thirdLink} />
-        <Button onClick={() => {}}>Save</Button>
+  useEffect(() => {
+    getTweetIds();
+  }, []);
+
+  console.log(links);
+
+  const handleSave = useCallback(async (val: string, id: string) => {
+    try {
+        const resp = await request({
+          path: `/tw_post_links/${id}/`,
+          method: "POST",
+          body: {link: val}
+        });
+        if (resp) {
+
+        }
+      } catch (e) {
+        console.log(e);
+      }
+}, [request])
+
+  return (
+    <div className={s.Container}>
+      <h2 className={s.Title}>Twitter accounts</h2>
+      {!loading && (
+        <>
+          {Object.keys(links).map((linkId) => {
+            return (
+              <div className={s.InputBox}>
+                <Input
+                  placeholder="Tweet ID"
+                  onChange={(e: any) =>
+                    setLinks((p) => ({ ...p, [linkId]: e.target.value }))
+                  }
+                  value={links[linkId]}
+                />
+                <Button onClick={() => handleSave(links[linkId], linkId)}>Save</Button>
+              </div>
+            );
+          })}
+        </>
+      )}
+      {loading && <h2 className={s.Loader}>Loading...</h2>}
     </div>
-}
+  );
+};
 
-export default TwitterWidgetsModal
+export default TwitterWidgetsModal;
